0e3857de06372e9358d5f2bf7c653951002561bf
kamiwares <kamil@waresiak.it>
2026-01-03 17:58:21 +0100

    retactor: media permissions

--- src/collections/Media.ts
+++ src/collections/Media.ts
@@ -3,14 +3,8 @@ import type { CollectionConfig } from 'payload'
 export const Media: CollectionConfig = {
   slug: 'media',
   access: {
-    read: ({ req }) => {
-      if (req?.user) return true
-      return req.headers.get('x-internal-token') === process.env.PAYLOAD_API_KEY
-    },
-    create: ({ req }) => {
-      if (req?.user) return true
-      return req.headers.get('x-internal-token') === process.env.PAYLOAD_API_KEY
-    },
+    read: () => true,
+    create: () => true,
   },
   fields: [
     {
